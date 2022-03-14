97fb044421670b5a397b73594eac4be529dab474
joe-lin-tech <lin0joe24@gmail.com>
2022-03-13 23:41:25 -0700

    Update Gatsby Node

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    actions.setWebpackConfig({
+        externals: getConfig().externals.concat(function (context, request, callback) {
+            const regex = /^@?firebase(\/(.+))?/;
+            if (regex.test(request)) {
+                return callback(null, `umd ${request}`);
+            }
+            callback();
+        }),
+    });
+};
--- webpack.server.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    resolve: {  
-        alias: {
-            ['firebase/app']: path.resolve(__dirname, 'node_modules/firebase/app/dist/index.cjs.js')
-        },
-        extensions: ['.ts', '.js']
-    },
-}
