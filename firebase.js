27f31b40422c753346d2fcb9625bbfab29125e32
joe-lin-tech <lin0joe24@gmail.com>
2022-03-14 00:06:23 -0700

    Update Firebase Hooks

--- firebase.js
+++ firebase.js
@@ -1,4 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
 
-export const app = initializeApp(firebaseConfig);
+
+// export const app = initializeApp(firebaseConfig);
+
+let instance
+
+export function getFirebase() {
+    if (typeof window !== 'undefined') {
+        if (instance) return instance;
+        instance = initializeApp(firebaseConfig);
+        return instance;
+    }
+    return null;
+}
--- src/hooks/firebase.js
+++ src/hooks/firebase.js
@@ -1,20 +1,22 @@
-import { app } from "../../firebase.js";
+// import { app } from "../../firebase.js";
+import { getFirebase } from "../../firebase.js";
 import { getAuth } from "firebase/auth"
 import { getFirestore, collection as firestoreCollection, doc, getDoc } from "firebase/firestore"
 import { useAuthState as getAuthState } from 'react-firebase-hooks/auth';
 import { useCollection as getCollection } from 'react-firebase-hooks/firestore';
 
 export const useAuth = () => {
-    return getAuth(app)
+    // return getAuth(app)
+    return getAuth(getFirebase())
 }
 
 export const useAuthState = () => {
-    console.log("useAuthState", getAuth(app))
     return getAuthState(useAuth())
 }
 
 export const useFirestore = () => {
-    return getFirestore(app)
+    // return getFirestore(app)
+    return getFirestore(getFirebase())
 }
 
 export const useCollection = (collection) => {
