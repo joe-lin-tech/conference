e643d96f6574cc1727de84135000cada6fcb936c
joe-lin-tech <lin0joe24@gmail.com>
2022-03-13 23:52:24 -0700

    Fix Gatsby Node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
     actions.setWebpackConfig({
-        externals: getConfig().externals.concat(function (context, request, callback) {
-            const regex = /^@?firebase(\/(.+))?/;
-            if (regex.test(request)) {
-                return callback(null, `umd ${request}`);
-            }
-            callback();
-        }),
-    });
-};
+      module: {
+        rules: [
+          {
+            test: /firebase/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
