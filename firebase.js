6abaa0b033cdb756a4fe41c9aeca5ecfd714ef66
joe-lin-tech <lin0joe24@gmail.com>
2022-03-13 23:32:36 -0700

    Update Firebase + Gatsby Node

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
 import { firebaseConfig } from "./config";
 
-export const app = initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig);
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,13 @@
-exports.onCreateWebpackConfig = ({
-  stage,
-  actions,
-  getConfig
-}) => {
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
-      externals: getConfig().externals.concat(function (context, request, callback) {
-        const regex = /^@?firebase(\/(.+))?/;
-        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+      externals: getConfig().externals.concat((context, request, callback) => {
+        const regex = /^@?firebase(\/)app(\/(.+))?/;
         if (regex.test(request)) {
-          return callback(null, 'umd ' + request);
+          return callback(null, `umd ${request}`);
         }
         callback();
-      })
+      }),
     });
   }
 };
