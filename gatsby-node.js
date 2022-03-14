79d5ed7ae8213e3e023636d9c1cb049ae45951d2
joe-lin-tech <lin0joe24@gmail.com>
2022-03-14 00:23:04 -0700

    Fix Gatsby Node Bugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,25 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /firebase/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   if (stage === "build-html") {
+//     actions.setWebpackConfig({
+//       module: {
+//         rules: [
+//           {
+//             test: /firebase/,
+//             use: loaders.null(),
+//           },
+//         ],
+//       },
+//     })
+//   }
+// }
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  actions.setWebpackConfig({
+    externals: getConfig().externals.concat(function(context, request, callback) {
+      const regex = /^@?firebase(\/(.+))?/;
+      if (regex.test(request)) {
+        return callback(null, `umd ${request}`);
+      }
+      callback();
+    }),
+  });
+};
