6ffe39723cbb7862181570e21462bb97d6329c1e
joe-lin-tech <lin0joe24@gmail.com>
2022-03-14 00:29:41 -0700

    Update Firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
 import { firebaseConfig } from "./config";
 
 
@@ -9,7 +10,8 @@ let instance
 export function getFirebase() {
     if (typeof window !== 'undefined') {
         if (instance) return instance;
-        instance = initializeApp(firebaseConfig);
+        // instance = initializeApp(firebaseConfig);
+        instance = firebase.initializeApp(firebaseConfig);
         return instance;
     }
     return null;
