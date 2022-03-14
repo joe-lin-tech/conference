6412fc0a34c8f0ed1e440bd999f535b80b5270c4
joe-lin-tech <lin0joe24@gmail.com>
2022-03-14 00:31:42 -0700

    Update Firebase Setup

--- firebase.js
+++ firebase.js
@@ -1,18 +1,5 @@
-// import { initializeApp } from "firebase/app";
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
 
 
-// export const app = initializeApp(firebaseConfig);
-
-let instance
-
-export function getFirebase() {
-    if (typeof window !== 'undefined') {
-        if (instance) return instance;
-        // instance = initializeApp(firebaseConfig);
-        instance = firebase.initializeApp(firebaseConfig);
-        return instance;
-    }
-    return null;
-}
+export const app = initializeApp(firebaseConfig);
