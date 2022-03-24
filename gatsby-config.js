db59e1a133394e87f5a70a862d77bc8d01b93a9a
joe-lin-tech <lin0joe24@gmail.com>
2022-03-24 09:06:06 -0700

    Update Google Analytics Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        ]
       }
     }
   ],
