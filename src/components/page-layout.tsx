059fae8325324ca32dab49e3582f58716dc981e3
joe-lin-tech <lin0joe24@gmail.com>
2022-04-05 12:15:11 -0700

    Create SEO Component

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import '../styles/global.css';
 import Footer from "./footer";
 import Navigation from './navigation';
+import SEO from "@bradgarropy/gatsby-plugin-seo"
 // import { useAuthState } from "../hooks/firebase";
 
 const PageLayout = ({ page, children }) => {
   return (
     <main>
+      <SEO
+        title="Autonomous Vehicle Expo"
+        description="Learn about the engineering, ethics, and policymaking of emerging autonomous vehicle technology."
+      />
       <Navigation page={page} />
       {children}
       <Footer />
